b5c05208daec319949a9e07803653803275f38e7
vagrant <bahar.hosseini@gmail.com>
2022-08-04 20:53:34 +0000

    handling reguest errors

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,18 +2,19 @@ const request = require('request');
 
 const args = process.argv.slice(2);
 request('https://api.thecatapi.com/v1/breeds', function(error, response, body) {
-  // console.error('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
+  if (error) {
+    console.error('error:', error);
+  } // Print the error if one occurred
+
   const data = JSON.parse(body);
   
-  let result = ''
+  let result = '';
   for (let item of data) {
     if (item['name'] === args[0]) {
-      result = item['description']
-      
+      result = item['description'];
+      break;
     }
     result = (`${args[0]} was not found!`);
   }
-  console.log(result)
+  console.log(result);
 });
